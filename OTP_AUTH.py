99f70109d67d0053d634ccb65cedaafd0a2e69a6
fabriziomonge <37080465+fabriziomonge@users.noreply.github.com>
2020-08-12 10:11:14 +0200

    Add files via upload
    Upload Pythonapp

---
+++ OTP_AUTH.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[15]:
+
+
+import streamlit as st
+
+number = input("inserire il proprio numero di cellulare per autenticazione") #jupytercode
+
+# number = st.number_input("inserire il proprio numero di cellulare per autenticazione", 4) #streamlit code
+
+number = "0039"+number
+
+if number != 4:
+    
+
+    # generate random integer values
+    from random import seed
+    from random import randint
+    # seed random number generator
+    
+    # generate some integers
+
+    value = randint(1000, 9999)
+    value = str(value)
+
+    from textmagic.rest import TextmagicRestClient
+  
+    username = "fabriziomonge"
+    token = "FXYszXlUIdZKW3xVC8da90mTm30RME"
+    client = TextmagicRestClient(username, token)
+    # weYT0IOJ1c api password
+  
+    message = client.messages.create(phones=number, text=value)
+
+# PSW = st.text_input("Inserire il codice ricevuto tramite sms") #streamlit code
+PSW = input("Inserire il codice ricevuto tramite sms") #jupytercode
+
+if PSW == value:
+    print("Autenticazione riuscita") #jupytercode
+    
+    st.write("""
+    # Autenticazione effettuata.
+    """) #streamlit code
+    
+    autorizzazione = "OK"
+    
+else:
+    print("Autenticazione fallita!") #jupytercode
+
+    st.write("""
+    # Autenticazione fallita.
+    """) #streamlit code
+    
+    autorizzazione = "NO"
+    
+
+
+# In[13]:
+
+
+number
+
+
+# In[ ]:
+
+
+
+
