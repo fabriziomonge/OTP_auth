ee4ed947d6c3cc444ce0fb1e75f6beb2dc49c518
fabriziomonge <37080465+fabriziomonge@users.noreply.github.com>
2020-08-12 18:49:17 +0200

    Update OTP_AUTH.py

--- OTP_AUTH.py
+++ OTP_AUTH.py
@@ -1,3 +1,6 @@
+
+
+
 import streamlit as st
 from textmagic.rest import TextmagicRestClient
 from random import seed
@@ -6,12 +9,17 @@ username = "fabriziomonge"
 token = "FXYszXlUIdZKW3xVC8da90mTm30RME"
 client = TextmagicRestClient(username, token)
 
+
+st.title("Applicazione per convelida accessi con OTP")
+
 inizio = 1000
 
 fine = 9999
 
 
 # number = input("inserire il proprio numero di cellulare per autenticazione") #jupytercode
+
+st.write("## Questa applicazione permette di convalidare un utente tramite una password numerica di quattro caratteri inviata tramite sms sul cellulare dell' utente")
 
 number = st.text_input("inserire il proprio numero di cellulare per autenticazione", "4") #streamlit code
 
@@ -32,15 +40,17 @@ if number != "00394":
     
     generator(inizio,fine,number)
     value=generator(inizio,fine,number)[0]
-    PSW = st.text_input("Inserire il codice ricevuto tramite sms", "in attesa")
+    st.write("## Controlla il telefono")
     
-    if PSW == "in attesa":
+    PSW = st.text_input("Inserire il codice ricevuto tramite sms e premere invio", "")
+    
+    if PSW == "":
         st.write("In attesa del codice di conferma")
     else:
         if PSW == value:
             
-            st.write("Convalidato")
+            st.write("# Utente Convalidato")
         else:
             
-            st.write("Accesso negato")
+            st.write("# Accesso negato")
     
